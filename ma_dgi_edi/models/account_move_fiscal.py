2b3f3841aa307ac5ca63c79384343f0c54f9fa76
amhine <amhinenihad@gmail.com>
2025-06-18 15:53:00 +0100

    remplire les models

---
+++ ma_dgi_edi/models/account_move_fiscal.py
@@ -0,0 +1,8 @@
+
+from odoo import models, fields
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    included_in_declaration = fields.Boolean(string='Inclure dans la Déclaration Fiscale', default=True)
+
---
+++ ma_dgi_edi/models/edi_document.py
@@ -0,0 +1,19 @@
+
+from odoo import models, fields
+
+class EdiDocument(models.Model):
+    _name = 'edi.document'
+    _description = 'Document EDI'
+
+    name = fields.Char(string='Nom', required=True)
+    declaration_id = fields.Many2one('fiscal.declaration', string='Déclaration liée', required=True, ondelete='cascade')
+    document_type = fields.Selection([
+        ('bilan', 'Bilan'),
+        ('cpc', 'Compte de Produits et Charges'),
+        ('esg', 'État des Soldes de Gestion'),
+        ('tf', 'Tableau de Financement')
+    ], string='Type', required=True)
+    file_name = fields.Char(string='Nom de fichier XML')
+    xml_content = fields.Text(string='Contenu XML')
+    
+    
